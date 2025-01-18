ce2be71de133aaa24347b79178bd890a0376b5be
A-F-V <faracea2000@gmail.com>
2025-01-18 12:59:34 +0000

    Tailwind Baby

--- src/ui/view.tsx
+++ src/ui/view.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function JanusView() {
-	return <h1>Hello</h1>;
+	return <h1 className="text-primary">Hello</h1>;
 }
---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["src/ui/**/*.tsx"],
+	corePlugins: {
+		preflight: false,
+	},
+	theme: {
+		extend: {
+			colors: {
+				border: "var(--text-normal)",
+				background: "var(--background-primary)",
+				foreground: "var(--text-normal)",
+				primary: {
+					DEFAULT: "var(--color-orange)",
+					foreground: "var(--text-normal)",
+				},
+				secondary: {
+					DEFAULT: "var(--color-blue)",
+					foreground: "var(--secondary-foreground)",
+				},
+				destructive: {
+					DEFAULT: "var(--color-red)",
+					foreground: "var(--text-normal)",
+				},
+				muted: {
+					DEFAULT: "var(--background-secondary)",
+					foreground: "var(--text-muted)",
+				},
+				accent: {
+					DEFAULT:
+						"hsl(var(--accent-h) var(--accent-s) var(--accent-l))",
+					foreground: "var(--text-on-accent)",
+				},
+			},
+			borderRadius: {
+				xl: "var(--radius-xl)",
+				lg: "var(--radius-lg)",
+				md: "var(--radius-m)",
+				sm: "var(--radius-s)",
+			},
+		},
+	},
+	plugins: [],
+};
